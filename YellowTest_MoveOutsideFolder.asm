b8cdb01d9052e21be4a935a6f3105a0ca8b75288
Manos <138156574+M4n0zz@users.noreply.github.com>
2025-03-26 00:46:19 +0200

    Update YellowTest_MoveOutsideFolder.asm

--- YellowTest_MoveOutsideFolder.asm
+++ YellowTest_MoveOutsideFolder.asm
@@ -10,29 +10,29 @@ SECTION "ItemGiverTest", ROM0
 LOAD "ItemGiver", WRAMX[$D8B4]  ; $D8B5 for rb
 start:
 call ClearScreen
-ld a, 255		    ; total item IDs
+ld a, 255		        ; total item IDs
 ld [wMaxItemQuantity],a 
 call DisplayChooseQuantityMenu
-and a, a		    ; if a is 0, z flag is set
-ret nz 			    ; if B pressed, then ret
+and a, a		         ; if a is 0, z flag is set
+ret nz 			         ; if B pressed, then ret
 ld a, [wItemQuantity]
 push af
 ld [wNamedObjectIndex], a
 call GetItemName 
-ld hl, $c409	    ; destination
+ld hl, $c409	      ; destination
 ld de, wNameBuffer	; origin
 call CopyString
 dec hl
-ld [hl], $7f	    ; blank last name byte
+ld [hl], $7f	      ; blank last name byte
 ld a, 99
 ld [wMaxItemQuantity], a
 call DisplayChooseQuantityMenu
 pop bc
-and a, a		    ; if a is 0, z flag is set
+and a, a		       ; if a is 0, z flag is set
 jr nz, start	    ; if B pressed go to the beginning, ln0
 ld a, [wItemQuantity]
-ld c, a			    ; bc = id, quantity
+ld c, a			       ; bc = id, quantity
 call GiveItem
-jr start		    ; jp to start, ln0
+jr start		       ; jp to start, ln0
 ENDL
  
